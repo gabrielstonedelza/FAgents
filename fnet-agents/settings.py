1b5384f637ffb796ca047f1487ea80030c1074ed
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-04-19 12:37:50 +0000

    fnet agents app,added server address

--- fnet-agents/settings.py
+++ fnet-agents/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.fnetagents.xyz','165.227.220.10','localhost', '127.0.0.1']
 
 
 # Application definition
