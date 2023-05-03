a2793210aa57e3376b627f4057e66f111b14032b
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-05-03 11:26:40 +0000

    easy agent api,added customer picture to customer model

--- api/models.py
+++ api/models.py
@@ -127,12 +127,12 @@ NOTIFICATIONS_TRIGGERS = (
 class Customer(models.Model):
     agent = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=150, blank=True)
-    location = models.CharField(max_length=100, blank=True)
-    digital_address = models.CharField(max_length=25, blank=True)
+    phone = models.CharField(max_length=15, unique=True, blank=True)
+    # location = models.CharField(max_length=100, blank=True)
+    # digital_address = models.CharField(max_length=25, blank=True)
     # id_type = models.CharField(max_length=50, choices=ID_TYPES, blank=True, default="Passport")
     # id_number = models.CharField(max_length=50, blank=True, default="")
-    phone = models.CharField(max_length=15, unique=True, blank=True)
-    date_of_birth = models.CharField(max_length=15, blank=True)
+    # date_of_birth = models.CharField(max_length=15, blank=True)
     customer_pic = models.ImageField(upload_to="customer_pics",default="")
     date_created = models.DateTimeField(auto_now_add=True)
 
