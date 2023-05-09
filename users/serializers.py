8b6ee7dee877130b2cbfbc7e72c90298ae1778ad
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-05-09 10:58:31 +0000

    easy agent api,adding and removing agents from the blocked lists

--- users/serializers.py
+++ users/serializers.py
@@ -12,7 +12,7 @@ class UserCreateSerializer(UserCreateSerializer):
 class UsersSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'username', 'phone_number', 'full_name','agent_unique_code','supervisor']
+        fields = ['id', 'email', 'username', 'phone_number', 'full_name','agent_unique_code','supervisor','user_blocked']
 
 
 class SupervisorProfileSerializer(serializers.ModelSerializer):
