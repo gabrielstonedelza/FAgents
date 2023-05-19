fa5a78efebe371e1364c40259c4e3d1ca453563e
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-05-19 08:31:05 +0000

    easy agent api,added owners to momo deposit and withdraw,bank deposit and withdraw

--- api/serializers.py
+++ api/serializers.py
@@ -119,7 +119,7 @@ class MomoWithdrawalSerializer(serializers.ModelSerializer):
 class BankWithdrawalSerializer(serializers.ModelSerializer):
     class Meta:
         model = BankWithdrawal
-        fields = ['id','agent','onwer','customer','bank','withdrawal_type','amount','date_of_withdrawal','get_agents_phone','get_agent_username','d_200','d_100','d_50','d_20','d_10','d_5','d_2','d_1','total']
+        fields = ['id','agent','owner','customer','bank','withdrawal_type','amount','date_of_withdrawal','get_agents_phone','get_agent_username','d_200','d_100','d_50','d_20','d_10','d_5','d_2','d_1','total']
         read_only_fields = ['agent']
 class PaymentForReBalancingSerializer(serializers.ModelSerializer):
     class Meta:
