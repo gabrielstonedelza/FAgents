e10c4ec68c258305d43287e3caf1923ccdf34ac4
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-06-20 14:09:52 +0000

    easy agent api

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from .models import User, OwnerProfile, AdminProfile, AgentProfile
 
+class AdminUserProfile(admin.ModelAdmin):
+    list_display = ['id','user_type','email','full_name','phone_number','username']
+    search_fields = ['id','user_type','email','full_name','phone_number','username']
+
 admin.site.register(User)
 admin.site.register(OwnerProfile)
 admin.site.register(AdminProfile)
