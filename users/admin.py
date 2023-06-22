c5712fafc4b8d3628be8eef59361850915a3fc70
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-06-22 09:44:50 +0000

    easy agent api, changing all owner code to owner usernames

--- users/admin.py
+++ users/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import User, OwnerProfile, AdminProfile, AgentProfile
 
 class AdminUserProfile(admin.ModelAdmin):
-    list_display = ['id','user_type','email','full_name','phone_number','username','owner']
-    search_fields = ['id','user_type','email','full_name','phone_number','username','owner']
+    list_display = ['id','user_type','email','full_name','phone_number','username','owner','agent_unique_code']
+    search_fields = ['id','user_type','email','full_name','phone_number','username','owner','agent_unique_code']
 
     class Meta:
         model = User
