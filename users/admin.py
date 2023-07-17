ed47cccb21be00efaea5200243ba7021dd1badcc
Gabriel Akwasi Asare <gabrielstonedelza@gmail.com>
2023-07-17 12:38:04 +0000

    easy agent api,added company name to admin model

--- users/admin.py
+++ users/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import User, OwnerProfile, AdminProfile, AgentProfile
 
 class AdminUserProfile(admin.ModelAdmin):
-    list_display = ['id','user_type','email','full_name','phone_number','username','owner','agent_unique_code']
-    search_fields = ['id','user_type','email','full_name','phone_number','username','owner','agent_unique_code']
+    list_display = ['id','user_type','email','full_name','phone_number','username','owner','agent_unique_code','company_name']
+    search_fields = ['id','user_type','email','full_name','phone_number','username','owner','agent_unique_code','company_name']
 
     class Meta:
         model = User
